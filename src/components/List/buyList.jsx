936a72312aeec9388ecc7d82e7f7bc406ef962e7
matin faraji <matinfy1380@gmail.com>
2024-06-07 01:26:16 +0330

    card change

--- src/components/List/buyList.jsx
+++ src/components/List/buyList.jsx
@@ -1,126 +1,32 @@
+// import React from 'react';
 import "./buyList.css";
+import db from "../db/data.json";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 export default function Card() {
   return (
     <div className="allCard">
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/cactus2-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
+      {db.map((item, index) => (
+        <div className="card" key={index} style={{ position: "relative" }}>
+          <img src={item.src} alt="" />
+          <p className="p1">{item.type}</p>
+          <p className="p2">{item.name}</p>
+          <p className="price">${item.price}</p>
+          <button className="buy-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-bag-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4z" />
+            </svg>
+          </button>
         </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
-      {/* kldsjncjasn */}
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/cactus4-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
-        </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
-      {/* kldsjncjasn */}
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/cactus6-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
-        </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
-      {/* kldsjncjasn */}
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/plant1-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
-        </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
-      {/* kldsjncjasn */}
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/plant3-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
-        </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
-      {/* kldsjncjasn */}
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/plant4-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
-        </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
-      {/* kldsjncjasn */}
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/plant5-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
-        </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
-      {/* kldsjncjasn */}
-      <div className="card">
-        <div className="card-image">
-          <img
-            src="https://websitedemos.net/plant-store-02/wp-content/uploads/sites/410/2019/01/plant6-free-img.jpg"
-            alt="Boncellensis Secullant"
-          />
-          <button className="buy-button">خرید</button>
-        </div>
-        <div className="card-content">
-          <h2>Plants</h2>
-          <p>Boncellensis Secullant</p>
-          <div className="card-price">$34.00</div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
